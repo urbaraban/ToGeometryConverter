ac93e1dc41b0a0ff85d25e02f2d170e294ae99f9
urbaraban <e.shadrunov@mail.ru>
2025-09-22 20:55:33 +0700

    Выпилил весь рендр с устройства и пернес в модули

---
+++ Format/DWG.cs
@@ -0,0 +1,30 @@
+﻿using ACadSharp;
+using ACadSharp.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ToGeometryConverter.Object;
+
+namespace ToGeometryConverter.Format
+{
+    public class DWG : GCFormat
+    {
+        public DWG() : base("DWG", new string[1] { ".dwg" }) { }
+
+        public override Get ReadFile => GetAsync;
+
+        private async Task<object> GetAsync(string filename)
+        {
+            CadDocument doc = DwgReader.Read(filename, onNotification);
+
+            return null;
+        }
+
+        private static void onNotification(object sender, NotificationEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
